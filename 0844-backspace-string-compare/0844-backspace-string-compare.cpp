3041528dbb159f5a92d058a30f61cdaab4a504ab
Usman Gill <160505371+USMANGILL265@users.noreply.github.com>
2024-08-16 16:08:20 +0500

    Time: 0 ms (100.00%), Space: 7.8 MB (55.90%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string first = "";
+        int count = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '#') {
+                count++;
+                continue;
+            }
+            if (count > 0) {
+                count--;
+            } else {
+                first += s[i];
+            }
+        }
+        count = 0;
+        string sec = "";
+        for (int i = t.size() - 1; i >= 0; i--) {
+            if (t[i] == '#') {
+                count++;
+                continue;
+            }
+            if (count > 0) {
+                count--;
+            } else {
+                sec += t[i];
+            }
+        }
+
+        return first == sec;
+    }
+};
